ac3fe7d8146f711a08284df7aadab29fcfb64318
bencepapp64 <bence4436@gmail.com>
2023-01-31 20:40:35 +0100

    Fájlbeolvasás és statusbar

--- NevGenerator/MainWindow.xaml.cs
+++ NevGenerator/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
 
 namespace NevGenerator
 {
@@ -20,9 +23,37 @@ namespace NevGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<String> csaladiNevek = new ObservableCollection<string>();
+        private ObservableCollection<String> utoNevek = new ObservableCollection<string>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnCsBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog()==true)
+            {
+                foreach (var item in File.ReadAllLines(ofd.FileName).ToList())
+                {
+                    csaladiNevek.Add(item);
+                }
+                lbxCsNevek.ItemsSource = csaladiNevek;
+            }
+        }
+
+        private void btnUtBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                foreach (var item in File.ReadAllLines(ofd.FileName).ToList())
+                {
+                    utoNevek.Add(item);
+                }
+                lbxUtNevek.ItemsSource = utoNevek;
+            }
         }
     }
 }
